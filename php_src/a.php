01bdd70d628b9be9f813ab2881a3a259c7cd7922
Prince Hemrajani <prince@Princes-MacBook-Pro.local>
2015-10-31 21:21:20 -0700

    added methods for geofence updata and delete

--- php_src/a.php
+++ php_src/a.php
@@ -1,4 +1,3 @@
-
 <?php
 $host = "host=eventos.cyeijrrcwqvl.us-west-2.rds.amazonaws.com";
 $port = "port=5590";
@@ -61,7 +60,7 @@ if (!$db)
 					$result['success'] = 'false';
 					$result['error'] = 'Invalid or missing geofenceidId';
 					echo json_encode($result);
-        }
+					}
 
 				break;
 
@@ -123,6 +122,123 @@ if (!$db)
 				}
 			}
 		}
+
+	if ($_SERVER['REQUEST_METHOD'] == 'PUT')
+		{
+		if (isset($_REQUEST['selector']))
+			{
+			$selector = $_REQUEST['selector'];
+			switch ($selector)
+				{
+			case 6:
+				if (isset($_REQUEST['geofenceid']) && isset($_REQUEST['geofencename']) && isset($_REQUEST['long']) && isset($_REQUEST['lat']) && isset($_REQUEST['radius']))
+					{
+					updategeofence($_REQUEST['geofenceid'], $_REQUEST['geofencename'], $_REQUEST['radius'], $_REQUEST['long'], $_REQUEST['lat'], $db);
+					}
+				  else
+					{
+					$result = array();
+					$result['success'] = 'false';
+					$result['error'] = 'Some data is missing';
+					echo json_encode($result);
+					}
+
+				break;
+
+			case 7:
+				if (isset($_REQUEST['geofenceid']))
+					{
+					deletegeofence($_REQUEST['geofenceid'], $db);
+					}
+				  else
+					{
+					$result = array();
+					$result['success'] = 'false';
+					$result['error'] = 'Some data is missing';
+					echo json_encode($result);
+					}
+
+				break;
+
+			default:
+				$result['success'] = 'false';
+				$result['error'] = 'Invalid selector value';
+				echo json_encode($result);
+				}
+			}
+		}
+	}
+
+function deletegeofence($geofenceid, $db)
+	{
+	$sql = "SELECT count(*) FROM geofence where geofenceid='" . $geofenceid . "'";
+	$ret = pg_query($db, $sql);
+	$row = pg_fetch_row($ret);
+
+	// echo $row[0];
+	// exit;
+
+	if ($row[0] == 0)
+		{
+		$result['success'] = 'false';
+		$result['error'] = 'No such geofence exits';
+		echo json_encode($result);
+		}
+	  else
+		{
+		$sql = "Delete from geofence where geofenceid = '" . $geofenceid . "'";
+
+		// echo $sql;
+
+		$ret = pg_query($db, $sql);
+		if (!$ret)
+			{
+			$result['success'] = 'false';
+			$result['error'] = pg_last_error($db);
+			echo json_encode($result);
+			}
+		  else
+			{
+			$result['success'] = 'True';
+			echo json_encode($result);
+			}
+		}
+	}
+
+function updategeofence($geofenceid, $geofencename, $radius, $long, $lat, $db)
+	{
+	$sql = "SELECT count(*) FROM geofence where geofenceid='" . $geofenceid . "'";
+	$ret = pg_query($db, $sql);
+	$row = pg_fetch_row($ret);
+
+	// echo $row[0];
+	// exit;
+
+	if ($row[0] == 0)
+		{
+		$result['success'] = 'false';
+		$result['error'] = 'No such geofence exits';
+		echo json_encode($result);
+		}
+	  else
+		{
+		$sql = "UPDATE geofence set geofencename='" . $geofencename . "',center=ST_GeographyFromText('SRID=4326;POINT(" . $long . " " . $lat . ")'),radius= '" . $radius . "' where geofenceid = '" . $geofenceid . "'";
+
+		// echo $sql;
+
+		$ret = pg_query($db, $sql);
+		if (!$ret)
+			{
+			$result['success'] = 'false';
+			$result['error'] = pg_last_error($db);
+			echo json_encode($result);
+			}
+		  else
+			{
+			$result['success'] = 'True';
+			echo json_encode($result);
+			}
+		}
 	}
 
 function displayevents($geofenceid, $db)
@@ -149,50 +265,54 @@ function displayevents($geofenceid, $db)
 			$geometry = json_decode($row[0]);
 			$radius = $row[1];
 			$geometry = get_object_vars($geometry);
-      // echo $geometry['coordinates'][0];
+
+			// echo $geometry['coordinates'][0];
 			// var_dump($radius);
 
-      $sql = "SELECT * FROM events WHERE ST_DWithin(location, ST_SetSRID(ST_Point(".$geometry['coordinates'][0].",".$geometry['coordinates'][1]."), 4326),".($radius * 1609).")";
+			$sql = "SELECT * FROM events WHERE ST_DWithin(location, ST_SetSRID(ST_Point(" . $geometry['coordinates'][0] . "," . $geometry['coordinates'][1] . "), 4326)," . ($radius * 1609) . ")";
+			// echo $sql;
+			$ret = pg_query($db, $sql);
 
-      echo $sql;
-      $ret = pg_query($db, $sql);
-      // exit;
-      if (pg_num_rows($ret) == 0)
-  			{
-  			$result['success'] = 'false';
-  			$result['error'] = "No record found";
-  			echo json_encode($result);
-  			}
-  		  else
-  			{
+			// exit;
 
-      while ($row = pg_fetch_row($ret))
+			if (pg_num_rows($ret) == 0)
 				{
+				$result['success'] = 'false';
+				$result['error'] = "No record found";
+				echo json_encode($result);
+				}
+			  else
+				{
+				while ($row = pg_fetch_row($ret))
+					{
+					$events[] =
+						array(
+							'eventid' => $row[0],
+							'eventname' => $row[1],
+							'description' => $row[2],
+							'logo_url' => $row[3],
+							'end_time' => $row[4],
+							'start_time' => $row[5],
+							'eventwebsiteurl' => $row[7],
+						
+					);
 
-			$events[] = array(
-				array(
-					'eventid' => $row[0] ,
-					'eventname' => $row[1],
-					'description' => $row[2],
-          'logo_url' => $row[3],
-          'end_time' => $row[4],
-          'start_time' => $row[5],
-          'eventwebsiteurl' => $row[7],
-				)
-			);
-      // break;
-    }
-			$result['success'] = 'True';
-      if($events)
-      {
-			$result['events'] = $events;
-    }
-    $result['events'] = $events;
-			echo json_encode($result);
-    }
+					// break;
+
+					}
+
+				$result['success'] = 'True';
+				if ($events)
+					{
+					$result['events'] = $events;
+					}
+
+				$result['events'] = $events;
+				echo json_encode($result);
+				}
+			}
 		}
 	}
-}
 
 function displaygeofence($userid, $db)
 	{
@@ -235,7 +355,7 @@ function displaygeofence($userid, $db)
 function addgeofence($userid, $geofencename, $radius, $long, $lat, $db)
 	{
 	$sql = "INSERT INTO geofence (userid,geofencename,center,radius) VALUES ('" . $userid . "','" . $geofencename . "', ST_GeographyFromText('SRID=4326;POINT(" . $long . " " . $lat . ")'),'" . $radius . "')";
-	echo $sql;
+	// echo $sql;
 	$ret = pg_query($db, $sql);
 	if (!$ret)
 		{
@@ -315,58 +435,3 @@ function userLogin($selector, $email, $password, $db)
 	}
 
 ?>
-
-
-<!-- if (isset($_GET['long']) && isset($_GET['lat']) && isset($_GET['radius'])) {
-  echo "long" . $_GET['long'] . "<br/><br/>";
-  echo "lat" . $_GET['lat'] . "<br/><br/>";
-  echo "radius" . $_GET['radius'] . "<br/><br/>";
-
-
-
-
-
-  //  $sql ="SELECT * FROM events WHERE ST_Distance_Sphere(location, ST_MakePoint(".$_GET['long'].",".$_GET['lat'].")) <= ".$_GET['radius'] ;
-
-
-
-
-
-  $sql = "SELECT st_astext(location) FROM events WHERE ST_DWithin(Geography(location),Geography(ST_MakePoint(" . $_GET['long'] . "," . $_GET['lat'] . "))," . $_GET['radius'] . ")";
-  $ret = pg_query($db, $sql);
-  if (!$ret) {
-    echo pg_last_error($db);
-    exit;
-  }
-
-  while ($row = pg_fetch_row($ret)) {
-
-
-
-
-
-    // echo  $row[0] . "\n";
-
-
-
-
-
-    echo $row[0] . "<br/><br/>";
-
-
-
-
-
-    // echo  $row[2] ."<br/><br/>";
-    // echo $row[3] ."<br/><br/>";
-    // echo $row[4] ."<br/><br/>";
-    // echo $row[5] ."<br/><br/>";
-    // echo $row[6] ."<br/><br/>";
-    // echo $row[7] ."<br/><br/>";
-    // echo $row[8] ."<br/><br/>";
-
-
-
-
-
-    echo "<br/><br/><br/><br/>"; -->
